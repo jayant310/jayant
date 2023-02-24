041e4797327f2f85aa0a5ceb2c86f5d6e76df46e
jayant <jay223860@gmail.com>
2023-02-24 20:58:02 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let hamIcon = document.querySelector(".icon")
+let menu = document.querySelector(".menu")
+
+function nav(){
+    hamIcon.classList.toggle("active")
+    menu.classList.toggle("active")
+}
+
